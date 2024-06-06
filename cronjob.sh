66e54c5722199e16e486f3a2f4b6de49e184a7c6
deepanshunagarro <136328411+deepanshunagarro@users.noreply.github.com>
2024-06-06 20:52:35 +0530

    Create cronjob.sh

---
+++ cronjob.sh
@@ -0,0 +1,2 @@
+az storage blob upload --account-name "deep1999a3e2" --account-key "sv=2022-11-02&ss=b&srt=co&sp=rwlacx&se=2024-06-06T23:21:40Z&st=2024-06-06T15:21:40Z&spr=https&sig=scLoQaWau8eAxX4ekSbH6hn5jHBQFqv05xrmnAPMTX0%3D" --container-name "test" --name "dummy.txt" --file "dummy.txt"
+
